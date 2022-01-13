f2efc3315fb97176fed323b5b91ac79f95b2c487
Justin Peisker <justinpeisker@gmail.com>
2022-01-13 09:51:43 -0800

    wip

---
+++ index.js
@@ -0,0 +1,23 @@
+const cohort1 = process.argv[2]
+const user = process.env.USER
+
+
+if (cohort1 === 'web-49') {
+  console.log(`${cohort1} is the best forever!`)
+} else {
+  console.log(`booooh cohort ${cohort1}`)
+}
+
+console.log(`the user is ${user}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'hey there'})
+})
+
+const port = 9000
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
